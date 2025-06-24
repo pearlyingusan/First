e3e7a6ba4ec4cb0dff9586de9b10bf359c5cb54e
Pearly I. <ingusan.pearl@gmail.com>
2025-06-24 14:37:44 +0800

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,18 @@
+let rosePrice: 8;
+let lilyPrice: 10;
+let tulipPrice: 2;
+
+let roseQuantity: 70;
+let lilyQuantity: 50;
+let tulipQuantity: 120;
+
+let roseValue = rosePrice * roseQuantity;
+let lilyValue = lilyPrice * lilyQuantity;
+let tulipValue = tulipPrice * tulipQuantity;
+
+let Total = roseValue + lilyValue + tulipValue;
+console.log("Rose - unit price:", rosePrice, ", quantity:", roseQuantity, ", value:", roseValue);
+console.log("Lily -  unit price:", lilyPrice, ", quantity:", lilyQuantity, ", value:", lilyValue);
+console.log("Tulip - unit price:", tulipPrice, ", quantity:", tulipQuantity, ", value:", tulipValue);
+console.log("Total:", Total);
+
